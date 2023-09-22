5bcb0f06fde7fd1ea4b910164b4940cefec43452
Diwash Mainali <diwashmainali900@gmail.com>
2023-09-22 10:36:42 +0545

    Removed all the old files of AKKA COOKBOOK

--- src/main/scala/AkkaCookbook/Chapter1/HelloAkkaActorSystem.scala
+++
@@ -1,7 +0,0 @@
-package AkkaCookbook.Chapter1
-
-import akka.actor.ActorSystem
-object HelloAkkaActorSystem extends App {
-  val actorSystem = ActorSystem("HelloAkka")  // Creating an object of actor system with name HelloAkka
-  println(actorSystem)
-}
